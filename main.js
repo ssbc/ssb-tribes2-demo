ed6cb8eea01f66fb91939378c0e67ee76f02e0b1
Jacob Karlsson <jacob.karlsson95@gmail.com>
2023-07-04 15:54:50 +0200

    Set up electron

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
