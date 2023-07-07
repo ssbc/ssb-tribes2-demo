3aba47e2e5c4cb9c2af532297b3140514e845b83
Jacob Karlsson <jacob.karlsson95@gmail.com>
2023-07-07 18:54:29 +0200

    Add list of groups

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-const { createElement: h } = require("react");
+const { createElement: h, useState } = require("react");
 const { whoami } = require("./ssb");
 const NewConnection = require("./ui/NewConnection");
 const CreateGroup = require("./ui/CreateGroup");
+const ListGroups = require("./ui/ListGroups");
 
 function App() {
+  const [viewedGroup, setViewedGroup] = useState(null);
+
+  console.log("viewedGroup:", viewedGroup);
+
   return h(
     "div",
     {},
     h("div", {}, "My ssb id: " + whoami()),
     h(NewConnection),
-    h(CreateGroup)
-    //h(ListGroups)
+    h(CreateGroup),
+    h(ListGroups, { setViewedGroup })
   );
 }
 
---
+++ src/ui/ListGroups.js
@@ -0,0 +1,55 @@
+const { useState, createElement: h, useEffect } = require("react");
+const pull = require("pull-stream");
+const { ssb } = require("../ssb");
+
+function ListGroups({ setViewedGroup }) {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    let drain;
+    pull(
+      ssb.tribes2.list({ live: true }),
+      (drain = pull.drain((group) => {
+        const groupSet = new Set(groups);
+
+        if (!groupSet.has(group.id)) {
+          groupSet.add(group.id);
+
+          setGroups([...groupSet]);
+        }
+      }))
+    );
+
+    return () => drain.abort();
+  });
+
+  return h(
+    "div",
+    { style: { margin: "1rem" } },
+    h("div", {}, h("span", {}, "Groups you're a member of:")),
+    h(
+      "ul",
+      {},
+      groups.map((groupId) =>
+        h(
+          "li",
+          { key: groupId, style: { marginBottom: "0.5rem" } },
+          h(
+            "a",
+            {
+              onClick: () => setViewedGroup(groupId),
+              style: {
+                color: "-webkit-link",
+                cursor: "pointer",
+                textDecoration: "underline",
+              },
+            },
+            groupId
+          )
+        )
+      )
+    )
+  );
+}
+
+module.exports = ListGroups;
